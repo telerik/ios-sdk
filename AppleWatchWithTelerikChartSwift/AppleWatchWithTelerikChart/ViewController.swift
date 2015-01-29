956d2e2f1d0012f95ee27f6f7262037a9fdd25de
nikolay-diyanov <nikolay.diyanov@telerik.com>
2015-01-29 19:04:46 +0200

    style updates

--- AppleWatchWithTelerikChartSwift/AppleWatchWithTelerikChart/ViewController.swift
+++ AppleWatchWithTelerikChartSwift/AppleWatchWithTelerikChart/ViewController.swift
@@ -9,24 +9,29 @@
 import UIKit
 import WatchCoreDataProxy
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, TKChartDelegate {
 
+    @IBOutlet weak var label: UILabel!
     let chart = TKChart()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let bounds = CGRectInset(self.view.bounds, 10, 10)
-        chart.frame = CGRectInset(CGRectMake(bounds.origin.x, bounds.origin.y, bounds.size.width, bounds.size.height / 2), 10, 10)
+        chart.frame = CGRectInset(CGRectMake(bounds.origin.x, bounds.origin.y + 100, bounds.size.width, bounds.size.height / 2), 10, 10)
+        chart.backgroundColor = UIColor.blackColor()
+        chart.delegate = self
         self.view.addSubview(chart)
         
         let array:NSMutableArray = [
-            TKChartDataPoint(name: "Google",value: 20),
-            TKChartDataPoint(name: "Apple",value: 30),
-            TKChartDataPoint(name: "Microsoft",value: 10),
-            TKChartDataPoint(name: "IBM",value: 5),
-            TKChartDataPoint(name: "Oracle",value: 8)]
+            TKChartDataPoint(name: "Germany",value: 20),
+            TKChartDataPoint(name: "USA",value: 30),
+            TKChartDataPoint(name: "Canada",value: 10),
+            TKChartDataPoint(name: "UK",value: 5),
+            TKChartDataPoint(name: "France",value: 8)]
         let series = TKChartPieSeries(items:array)
+        series.style.pointLabelStyle.textHidden = false
+        series.style.pointLabelStyle.textColor = UIColor.whiteColor()
         chart.addSeries(series)
         
         UIGraphicsBeginImageContext(chart.bounds.size)
@@ -37,12 +42,20 @@ class ViewController: UIViewController {
         WatchCoreDataProxy.sharedInstance.sharedAppGroup = "group.watch.sample"
 
         WatchCoreDataProxy.sharedInstance.sendImageToWatch(image)
-        WatchCoreDataProxy.sharedInstance.sendStringToWatch("Pie")
+        WatchCoreDataProxy.sharedInstance.sendStringToWatch(label.text!)
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func chart(chart: TKChart!, textForLabelAtPoint dataPoint: TKChartData!, inSeries series: TKChartSeries!, atIndex dataIndex: UInt) -> String! {
+        return dataPoint.dataName!()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
 }
 
