cac5ca443a8fd8065a7796fe7ef9049e6b1214a0
nikolay-diyanov <nikolay.diyanov@telerik.com>
2015-03-16 13:28:16 +0200

    update to Xcode 6.2

--- AppleWatchWithTelerikChartSwift/AppleWatchWithTelerikChart-Bridging-Header.h
+++ AppleWatchWithTelerikChartSwift/AppleWatchWithTelerikChart-Bridging-Header.h
@@ -2,4 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 #import <TelerikUI/TelerikUI.h>
